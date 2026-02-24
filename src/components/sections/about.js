a7c2aea7d7af364cbe07bffbdbde4d0505ace7f4
Nishanth R <nishusheela2011@gmail.com>
2026-02-23 21:53:34 -0500

    test image change

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -80,8 +80,8 @@ const StyledPic = styled.div`
     .img {
       position: relative;
       border-radius: var(--border-radius);
-      mix-blend-mode: normal;
-      filter: grayscale(35%) contrast(1.05);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1) brightness(90%);
       transition: var(--transition);
     }
 
